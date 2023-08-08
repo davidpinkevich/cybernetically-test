9369ca030821af8e3c30b81ab11f48b99bcd97d0
davidpinkevich <davidpinkevich95@gmail.com>
2023-08-08 20:21:31 +0300

    feat: add types for stocks

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,5 +1,41 @@
-export interface IInitialStore {
+export interface IInitialSymbols {
   amountSymbols: number;
   nameSymbols: Array<string>;
-  loadSymbols: string;
+  loadSymbols: string | null;
 }
+
+export interface IInitialStocks {
+  stocks: Array<IQuotes>;
+  loadStocks: string;
+  currentPage: number;
+  stars: Array<{ symbol: string; value: number }>;
+}
+
+export interface IGetData {
+  symbols: string;
+  types: string;
+  uniqueId: Array<string>;
+}
+
+export interface IQuotes {
+  index: number;
+  id: string;
+  companyName: string;
+  primaryExchange: string;
+  lastTradeTime: number;
+  iexCloseTime: number;
+  latestPrice: number;
+  iexClose: number;
+  change: number;
+  currency: string;
+  symbol: string;
+}
+
+export interface IItemsStocks {
+  items: Array<IQuotes>;
+}
+
+export type TStore = {
+  symbols: IInitialSymbols;
+  stocks: IInitialStocks;
+};
