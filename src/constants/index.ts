3682a0572cb47994c80f4ccfebdad1795c057e9c
davidpinkevich <davidpinkevich95@gmail.com>
2023-08-08 19:25:04 +0300

    feat: add constants for header table

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -2,3 +2,16 @@ export const URL = {
   BASE_URL: "https://cloud.iexapis.com/",
   API_KEY: "pk_07d01bfc88054e08ad70b2087c7c884f",
 };
+
+export const QUOTES_NAMES = [
+  "№",
+  "Company name",
+  "The primary listing exchange for the company",
+  "Time since the last trade on the market, excluding the auction closing trade",
+  "Exchange time for the close from IEX",
+  "The latest relevant price of the security which is derived from multiple sources",
+  "Close price from IEX",
+  "The price change between the last price and the previous close",
+  "Currency in which the prices are quoted",
+  "Your personal rating",
+];
